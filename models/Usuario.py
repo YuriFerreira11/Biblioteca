9567e407608860ac87579566d7d9c88bb5b0e549
YuriFerreira11 <yuriaromao@gmail.com>
2026-01-28 14:50:37 -0300

    Teste

---
+++ models/Usuario.py
@@ -0,0 +1,56 @@
+import os
+import json
+class Usuario:
+    lista = []
+    arquivo = "Nomes.json"
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+    @classmethod
+    def carregar_usuarios(cls):
+        if os.path.exists(cls.arquivo):
+            try:
+                with open(cls.arquivo, "r", encoding="utf-8") as f:
+                    cls.lista = json.load(f)
+            except json.JSONDecodeError:
+                cls.lista = []
+    @classmethod
+    def gerar_id(cls):
+        if not cls.lista:
+            return "0001"
+        ultimo_id = cls.lista[-1]["ID"]
+        novo_id = int(ultimo_id) + 1
+        return f"{novo_id:04d}"
+    def registrar_usuario(self):
+        if Usuario.procurar_usuario(self.nome):
+            print("Usuário ja cadastrado")
+            return
+        usuario = {
+            "Nome": self.nome,
+            "Idade": self.idade,
+            "ID": Usuario.gerar_id()
+        }
+        Usuario.lista.append(usuario)
+        Usuario.salvar_usuarios()
+
+    @classmethod
+    def salvar_usuarios(cls):
+        with open(cls.arquivo, "w", encoding="utf-8") as f:
+            json.dump(cls.lista, f, ensure_ascii=False, indent=4)
+
+    @classmethod
+    def procurar_usuario(cls, chave):
+        chave_lower = chave.lower()
+        for usuario in cls.lista:
+            if usuario["Nome"].lower() == chave_lower:
+                return usuario
+        return None
+    @classmethod
+    def listar_usuarios(cls):
+        return cls.lista
+    @classmethod
+    def verificar_usuario(cls, user_id):
+        for usuario in cls.lista:
+            if usuario["ID"] == user_id:
+                return usuario
+        return None
